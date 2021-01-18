352e5e6a3a0ac54bdfbdab66387fb4e206b599d8
tanoue232 <supright34@gmail.com>
2021-01-19 00:10:09 +0900

    task7-3復習(まだarticleに関しては疑問点あり)

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :coment, dependent: :destroy
   has_many :article_like, dependent: :destroy
-  validates :title, length: { maximum: 40 }, presence: true
+  validates :title, presence: true
 end
